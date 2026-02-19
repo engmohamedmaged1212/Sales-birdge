f6894fba2219a8c3211fe7ddb4eedf6c2b3e37f2
Mohamed Maged <eng.mohamed.maged.m@gmail.com>
2026-02-19 10:18:06 +0200

    Initial commit - Order Management System Backend

---
+++ src/main/java/com/qvc/orderflow/SalesbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.qvc.orderflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesbridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesbridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qvc/orderflow/SalesbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qvc.orderflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesbridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
